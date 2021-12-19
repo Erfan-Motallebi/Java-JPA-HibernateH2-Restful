3477f6f2b15c838faad836ee8b6da33cb7343467
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-19 23:56:28 +0330

    added the get all departments route / service

--- jpa.h2/src/main/java/com/springboot/jpa/h2/service/DepartmentService.java
+++ jpa.h2/src/main/java/com/springboot/jpa/h2/service/DepartmentService.java
@@ -1,7 +1,11 @@
 package com.springboot.jpa.h2.service;
 
+import java.util.List;
+
 import com.springboot.jpa.h2.entity.Department;
 
 public interface DepartmentService {
 	Department saveDepartment(Department department);
+
+	List<Department> extractAllDepartment();
 }
