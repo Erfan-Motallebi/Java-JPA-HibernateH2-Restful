0b7975134edaef57e231b4de7c8807967c9508ca
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-20 23:25:30 +0330

    added more Response Entity Check

---
+++ jpa.h2/src/main/java/com/springboot/jpa/h2/entity/EntitySuccessClass.java
@@ -0,0 +1,15 @@
+package com.springboot.jpa.h2.entity;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EntitySuccessClass{
+	private HttpStatus statusCode;
+	private String message;
+}
