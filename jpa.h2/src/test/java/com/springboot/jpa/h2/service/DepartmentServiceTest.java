06a02d9782a468bd22b85d8dd734618fbff7e297
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-21 11:07:57 +0330

    unit testing of service [findDepartmentByName  - IgnoreCase ]

---
+++ jpa.h2/src/test/java/com/springboot/jpa/h2/service/DepartmentServiceTest.java
@@ -0,0 +1,40 @@
+package com.springboot.jpa.h2.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.springboot.jpa.h2.entity.Department;
+import com.springboot.jpa.h2.repository.DepartmentRepository;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+	@Autowired
+	DepartmentService departmentService;
+
+	@MockBean
+	DepartmentRepository departmentRepository;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		Department department = Department.builder().departmentId(1L).departmentName("Engineering")
+				.departmentNumbers(10L).departmentSection("Software Engineering").departmentCode("EN-02").build();
+		
+		Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase(department.getDepartmentName())).thenReturn(department);
+	}
+
+	@Test
+	public void getTheDeparment_usingDepartmentName() {
+		String departmentName = "Marketing";
+		Department foundDep = departmentService.findDepartmentByName(departmentName);
+
+		assertEquals(departmentName, foundDep.getDepartmentName());
+	}
+
+}
